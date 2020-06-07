ebd6f7e8bc8f5b71a0f31ca09ecfce736f3fa7c6
Sulav <sulavdhakal96@gmail.com>
2020-06-07 21:13:28 +0545

    [this is a basic of python first try]

---
+++ first_class.py
@@ -0,0 +1,47 @@
+num= 3
+if num > 0:
+    print(num," is a positive number.")
+    print("I am inside the if statment")
+print("this is always printed")
+
+num=-1
+if num =>0:
+    print(num,"is a positive number.")
+print("this is also always printed.")
+
+num4= 4
+if num4>= 0:
+    print("this is postive or zero")
+else:
+    print ("this is a positive number")
+
+
+
+num5= 10
+if num5 >0:
+    print("it is positive")
+
+elif num5==0:
+    print("Zero")
+else:
+    print("its a negaative")
+
+
+
+number =[6,5,3,8,4,2,11]
+
+for num in number:
+    print(num)
+def asd:
+    string ="this is a test"
+    for i in string:
+        print(i)
+
+
+num=[1,2,3,4,5]
+count=0
+for i in num:
+    count += num
+    # count= num+i
+    # i++
+print("count")
